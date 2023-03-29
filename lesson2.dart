ddadcf732647071468c0b7cbf5026d8249146419
Liketravels <liketravelss@gmail.com>
2023-03-29 17:14:00 +0300

    prctice2

---
+++ lesson2.dart
@@ -0,0 +1,34 @@
+
+void main() {
+  num sum = 5;
+  sum = 5 + 1;
+
+print (sum);
+
+int temp = 30;
+print (temp);
+
+double pi = 3.56;
+print (pi);
+
+String msg = "Добро пожаловать 'во FLutter'";
+String msg1 = "Добро пожаловать";
+
+print (msg);
+
+List array = [ 1 , 2 , 3 ] ;
+
+print(array[2]);
+Map contacts = { 'name' : 'Sanjar', 'age' : 23 , 'Branch' : 'Computer Science' };
+
+
+var a = 10;
+print (a);
+
+var symbol = '\u2665';
+print (symbol);
+
+
+
+
+  }
